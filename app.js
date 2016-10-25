25975d80c4be222e876e984e655a1e59f74cfaa5
Christina Chan <christinaavchan@gmail.com>
2016-10-25 17:24:59 -0400

    POST request with Watson QAAPI to movies corpus

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var https = require('https');
 var bodyParser = require('body-parser');
 // var watson = require('watson-developer-cloud');
 var app = express();
@@ -9,8 +10,73 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(bodyParser.json())
 
 app.post('/question', function(req, res){
-  console.log(JSON.stringify(req.body));
-  res.send('it did a thing');
+  // credentials
+  var username = 'quk_student10';
+  var passwd = 'D8WLxiLk';
+
+  // Get the values
+  var syncTimeout = "1";
+  var query = req.body.question;
+
+  console.log("Timeout:" + syncTimeout + " Query:" + query);
+
+  // Set the required headers for posting the REST query to Watson
+  var headers = {
+    'Content-Type'  :'application/json',
+    'X-synctimeout' : syncTimeout,
+    'Authorization' : "Basic " + new Buffer("quk_student10"+":"+"D8WLxiLk").toString("base64")
+  };
+
+  // Create the request options to POST our question to Watson
+  var options = {
+    host: 'watson-wdc01.ihost.com',
+    port: '443',
+    path: '/instance/541/deepqa/v1/question',
+    method: 'POST',
+    headers: headers,
+    rejectUnauthorized: false, // ignore certificates
+    requestCert: true,
+    agent: false
+  };
+
+  var output = "";
+
+  // Create a request to POST to Watson
+  var post_req = https.request(options, function(result) {
+    result.setEncoding('utf-8');
+
+    // Retrieve and return the result back to the client            
+    result.on("data", function(chunk) {          
+      output += chunk; 
+    });
+
+    result.on('end', function(chunk) {    
+      // console.log(output);
+      var results = JSON.parse(output);
+      console.log(results);
+      res.send(results);
+    });
+  });
+
+
+  post_req.on('error',function(e) {
+    console.log("problem"+ e.message);
+  });
+
+  // create the Question text to ask Watson  
+  var question = {
+    question : {
+      questionText : query
+    }
+  };   
+  console.log(JSON.stringify(question));
+
+  // Set the POST body and send to Watson
+  post_req.write(JSON.stringify(question));
+  // post_req.write("\n\n");
+  post_req.end();
+
+  // res.send('it did a thing');
 });
 
 app.all('/*', function(req, res, next) {
--- public/app/home/home.js
+++ public/app/home/home.js
@@ -13,24 +13,33 @@ angular.module('watsonApp.home', ['ui.router'])
 .controller('HomeCtrl', function($http) {
   var homeCtrl = this;
 
-  homeCtrl.question = "";
+  homeCtrl.question = '';
   homeCtrl.answer = '';
+  homeCtrl.confidence = 0;
   homeCtrl.showAnswer = false;
+
+  homeCtrl.alertMsg = '';
+  homeCtrl.showAlert = false;
 
   homeCtrl.askWatson = function(){
     var json = {
-      "question": {
-        "questionText" : homeCtrl.question
-      }
-    }
+      "question": homeCtrl.question
+    };
     $http({
       method: 'POST',
       url: '/question',
       data: json
     }).then(function success(response) {
         console.log(response);
-        homeCtrl.answer = response.data;
-        homeCtrl.showAnswer = true;
+        if (response.data.question.answers.length > 0) {
+          var answer = response.data.question.evidencelist[0]
+          homeCtrl.answer = answer.text;
+          homeCtrl.confidence = answer.value;
+          homeCtrl.showAnswer = true;
+        } else {
+          homeCtrl.alertMsg = 'No answers for this question... sad face.';
+          homeCtrl.showAlert = true;
+        }
       }, function error(response) {
         console.log(response)
       });
