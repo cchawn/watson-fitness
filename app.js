b8928e7a51b2937b1f361593e3be880753f60243
Christina Chan <christinaavchan@gmail.com>
2016-10-25 12:52:15 -0400

    begin setting up app

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.listen(8080, function () {
+  console.log('Running on http://localhost:8080/');
+});
