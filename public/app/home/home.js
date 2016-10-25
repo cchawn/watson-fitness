bf437af0a852ffd97728887c672af684e2710618
Christina Chan <christinaavchan@gmail.com>
2016-10-25 16:31:01 -0400

    attempt with QAAPI

--- public/app/home/home.js
+++ public/app/home/home.js
@@ -3,13 +3,34 @@
 angular.module('watsonApp.home', ['ui.router'])
 
 .config(function($stateProvider) {
-  $stateProvider.state("home", {
+  $stateProvider.state('home', {
     url: '/',
     templateUrl: 'app/home/home.html',
     controller: 'HomeCtrl as ctrl'
   });
 })
 
-.controller('HomeCtrl', function() {
-  this.message = 'HOLA!';
+.controller('HomeCtrl', function($http) {
+  var homeCtrl = this;
+
+  homeCtrl.question = "";
+  homeCtrl.answer = '';
+  homeCtrl.showAnswer = false;
+
+  homeCtrl.askWatson = function(){
+    var json = {
+      "question": {
+        "questionText" : homeCtrl.question
+      }
+    }
+    $http({
+      method: 'POST',
+      url: '/question',
+      data: json
+    }).then(function success(response) {
+        console.log(response);
+      }, function error(response) {
+        console.log(response)
+      });
+  };
 });
